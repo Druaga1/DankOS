e465f04608f89c72b58cec69177739539a585e91
Mintz98 <the.mintlander@gmail.com>
2016-12-22 01:01:48 +0100

    Load root seems to work fine

---
+++ sources/fat.asm
@@ -0,0 +1,50 @@
+org 0x0100			; Program is loaded at offset 0x0100
+bits 16				; 16-bit real mode code
+
+push 0x07
+int 0x80
+
+push 0x03
+int 0x80
+
+xchg bx, bx
+
+mov bx, buffer
+mov dl, 0x00
+push 0x80
+int 0x80
+
+
+
+
+mov si, buffer
+
+mov cx, 256
+
+loops:
+
+xor eax, eax
+lodsb
+push 0x05
+int 0x80
+
+mov al, ' '
+push 0x01
+int 0x80
+
+loop loops
+
+
+
+
+
+
+
+push 0x00
+int 0x80
+
+
+times 512-($-$$) db 0x00
+
+
+buffer times 5000 db 0x00
