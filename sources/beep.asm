f72069f88b34eb944d9af1cb53c11db347631c20
Mintz98 <the.mintlander@gmail.com>
2017-01-25 11:38:30 +0100

    Music

--- sources/beep.asm
+++ sources/beep.asm
@@ -1,16 +1,31 @@
 org 0x100
 bits 16
 
-mov ecx, 440			; Play A 440hz
-push 0x22
-int 0x80
-
-mov ecx, 50				; Sleep 50 ticks
-push 0x1D
-int 0x80
-
-push 0x1E				; Stop speaker
+mov esi, music
+push 0x1F
 int 0x80
 
 push 0x00				; Return
 int 0x80
+
+
+
+
+music:
+
+; idk some music
+
+
+db 20			; Duration
+dw 440			; Note
+
+db 5
+dw 880
+
+db 5
+dw 0			; This is a pause
+
+db 10
+dw 440
+
+dw 0			; Song end marker
