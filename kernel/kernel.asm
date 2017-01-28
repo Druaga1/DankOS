d170ff28c8e0c95b183aa58b724185f1eb62d508
Mintz98 <the.mintlander@gmail.com>
2017-01-28 14:12:28 +0100

    Layout change

--- kernel/kernel.asm
+++ kernel/kernel.asm
@@ -59,10 +59,6 @@ int 0x80
 push 0x82				; Leave graphics mode (this should fix a bug on some machines)
 int 0x80
 
-mov si, SplashScreen	; Display SplashScreen (to be replaced with a graphical splash)
-push 0x02
-int 0x80
-
 reload:
 
 mov si, ShellName				; Use the default 'shell.bin'
@@ -105,101 +101,26 @@ ProcessWarning2	db	0x0A, "        The kernel will now reload 'shell.bin'."
 ShellSwitches	db	0x00
 BootDrive		db	0x00
 
-;Includes (internal routines)
+; ----- INTERNAL ROUTINES -----
 
-%include 'kernel/internal/timer_int.inc'
-%include 'kernel/internal/break_int.inc'
+%include 'kernel/internal/draw_cursor.inc'
+%include 'kernel/internal/clear_cursor.inc'
+%include 'kernel/internal/string_to_fat_name.inc'
+%include 'kernel/internal/erase_dir_cache.inc'
+%include 'kernel/internal/fat_delete_chain.inc'
+%include 'kernel/internal/fat_get_metadata.inc'
+%include 'kernel/internal/fat_load_chain.inc'
+%include 'kernel/internal/fat_load_root.inc'
+%include 'kernel/internal/fat_write_root.inc'
+%include 'kernel/internal/fat_name_to_string.inc'
+%include 'kernel/internal/fat_write_entry.inc'
+%include 'kernel/internal/path_converter.inc'
+%include 'kernel/internal/fat12_load_chain.inc'
+%include 'kernel/internal/fat12_delete_chain.inc'
+%include 'kernel/internal/floppy_read_sector.inc'
+%include 'kernel/internal/floppy_write_sector.inc'
 
-;Video
-
-%include 'kernel/internal/video/draw_cursor.inc'
-%include 'kernel/internal/video/clear_cursor.inc'
-
-;FAT
-
-%include 'kernel/internal/fat/string_to_fat_name.inc'
-%include 'kernel/internal/fat/erase_dir_cache.inc'
-%include 'kernel/internal/fat/fat_delete_chain.inc'
-%include 'kernel/internal/fat/fat_get_metadata.inc'
-%include 'kernel/internal/fat/fat_load_chain.inc'
-%include 'kernel/internal/fat/fat_load_root.inc'
-%include 'kernel/internal/fat/fat_write_root.inc'
-%include 'kernel/internal/fat/fat_name_to_string.inc'
-%include 'kernel/internal/fat/fat_write_entry.inc'
-%include 'kernel/internal/fat/path_converter.inc'
-
-;FAT12
-
-%include 'kernel/internal/fat/fat12/fat12_load_chain.inc'
-%include 'kernel/internal/fat/fat12/fat12_delete_chain.inc'
-
-;Disk
-
-%include 'kernel/internal/disk/floppy_read_sector.inc'
-%include 'kernel/internal/disk/floppy_write_sector.inc'
-
-;Includes (external routines)
-
-;Video
-
-%include 'kernel/external/video/enter_graphics_mode.inc'
-%include 'kernel/external/video/exit_graphics_mode.inc'
-%include 'kernel/external/video/draw_pixel.inc'
-%include 'kernel/external/video/draw_line.inc'
-%include 'kernel/external/video/draw_sprite.inc'
-%include 'kernel/external/video/clear_frame_buffer.inc'
-%include 'kernel/external/video/push_frame.inc'
-%include 'kernel/external/video/get_current_palette.inc'
-%include 'kernel/external/video/print_string.inc'
-%include 'kernel/external/video/center_print_string.inc'
-%include 'kernel/external/video/initialise_screen.inc'
-%include 'kernel/external/video/set_palette.inc'
-%include 'kernel/external/video/disable_cursor.inc'
-%include 'kernel/external/video/enable_cursor.inc'
-%include 'kernel/external/video/new_line.inc'
-%include 'kernel/external/video/get_cursor_position.inc'
-%include 'kernel/external/video/set_cursor_position.inc'
-%include 'kernel/external/video/put_char.inc'
-%include 'kernel/external/video/ascii_dump.inc'
-
-;Disk
-
-%include 'kernel/external/disk/floppy_read_sectors.inc'
-%include 'kernel/external/disk/floppy_read_word.inc'
-%include 'kernel/external/disk/floppy_read_byte.inc'
-%include 'kernel/external/disk/floppy_read_dword.inc'
-%include 'kernel/external/disk/floppy_write_sectors.inc'
-%include 'kernel/external/disk/floppy_write_word.inc'
-%include 'kernel/external/disk/floppy_write_byte.inc'
-%include 'kernel/external/disk/floppy_write_dword.inc'
-%include 'kernel/external/disk/invalid_cache.inc'
-%include 'kernel/external/disk/set_current_drive.inc'
-%include 'kernel/external/disk/get_current_drive.inc'
-
-;FAT
-
-%include 'kernel/external/fat/directory_scanner.inc'
-%include 'kernel/external/fat/get_current_dir.inc'
-%include 'kernel/external/fat/load_dir.inc'
-%include 'kernel/external/fat/load_file.inc'
-%include 'kernel/external/fat/ping_file.inc'
-%include 'kernel/external/fat/fat_time_to_integer.inc'
-
-;Kernel
-
-%include 'kernel/external/kernel/get_version_number.inc'
-%include 'kernel/external/kernel/terminate_process.inc'
-%include 'kernel/external/kernel/start_new_program.inc'
-%include 'kernel/external/kernel/allocate_memory.inc'
-%include 'kernel/external/kernel/allocate_mem32.inc'
-
-;Includes (global data and stuff)
-
-%include 'kernel/syscalls.inc'
-%include 'kernel/variables.inc'
-%include 'kernel/splash.inc'
-
-; TO BE SORTED
+; ----- EXTERNAL ROUTINES -----
 
 %include 'kernel/external/beep.inc'
 %include 'kernel/external/stop_beep.inc'
@@ -221,5 +142,53 @@ BootDrive		db	0x00
 %include 'kernel/external/cut_string.inc'
 %include 'kernel/external/get_char.inc'
 %include 'kernel/external/sleep.inc'
+%include 'kernel/external/enter_graphics_mode.inc'
+%include 'kernel/external/exit_graphics_mode.inc'
+%include 'kernel/external/draw_pixel.inc'
+%include 'kernel/external/draw_line.inc'
+%include 'kernel/external/draw_sprite.inc'
+%include 'kernel/external/clear_frame_buffer.inc'
+%include 'kernel/external/push_frame.inc'
+%include 'kernel/external/get_current_palette.inc'
+%include 'kernel/external/print_string.inc'
+%include 'kernel/external/center_print_string.inc'
+%include 'kernel/external/initialise_screen.inc'
+%include 'kernel/external/set_palette.inc'
+%include 'kernel/external/disable_cursor.inc'
+%include 'kernel/external/enable_cursor.inc'
+%include 'kernel/external/new_line.inc'
+%include 'kernel/external/get_cursor_position.inc'
+%include 'kernel/external/set_cursor_position.inc'
+%include 'kernel/external/put_char.inc'
+%include 'kernel/external/ascii_dump.inc'
+%include 'kernel/external/floppy_read_sectors.inc'
+%include 'kernel/external/floppy_read_word.inc'
+%include 'kernel/external/floppy_read_byte.inc'
+%include 'kernel/external/floppy_read_dword.inc'
+%include 'kernel/external/floppy_write_sectors.inc'
+%include 'kernel/external/floppy_write_word.inc'
+%include 'kernel/external/floppy_write_byte.inc'
+%include 'kernel/external/floppy_write_dword.inc'
+%include 'kernel/external/invalid_cache.inc'
+%include 'kernel/external/set_current_drive.inc'
+%include 'kernel/external/get_current_drive.inc'
+%include 'kernel/external/directory_scanner.inc'
+%include 'kernel/external/get_current_dir.inc'
+%include 'kernel/external/load_dir.inc'
+%include 'kernel/external/load_file.inc'
+%include 'kernel/external/ping_file.inc'
+%include 'kernel/external/fat_time_to_integer.inc'
+%include 'kernel/external/get_version_number.inc'
+%include 'kernel/external/terminate_process.inc'
+%include 'kernel/external/start_new_program.inc'
+%include 'kernel/external/allocate_memory.inc'
+%include 'kernel/external/allocate_mem32.inc'
+
+; ----- OTHER INCLUDES -----
+
+%include 'kernel/other/timer_int.inc'
+%include 'kernel/other/break_int.inc'
+%include 'kernel/other/syscalls.inc'
+%include 'kernel/other/variables.inc'
 
 times 0x8000-($-$$)			db 0x00				; Pad reserved sectors with 0x00
